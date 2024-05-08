b3ce25ddf80b5ca6bd04e766938594a71f84c32a
paulresmer <resmer.paul@gmail.com>
2024-05-08 04:49:03 -0400

    basic touch detection on capacitive touch slider working

--- firmware.c
+++ firmware.c
@@ -6,7 +6,35 @@
 #include "MKL46Z4.h"
 #include "fsl_debug_console.h"
 
+/* defs */
 #define SWITCH_1_PIN 3
+#define TOUCH_THRESHOLD 1000 // touch threshold (sens increases -> 0)
+
+void Touch_Init()
+{
+  SIM->SCGC5 |= SIM_SCGC5_TSI_MASK;
+
+  TSI0->GENCS = TSI_GENCS_OUTRGF_MASK | // out of range flag, set to 1 to clear
+                TSI_GENCS_MODE(0u) |
+                TSI_GENCS_REFCHRG(0u) |
+                TSI_GENCS_DVOLT(0u) |
+                TSI_GENCS_EXTCHRG(0u) |
+                TSI_GENCS_PS(0u) |
+                TSI_GENCS_NSCN(31u) |
+                TSI_GENCS_TSIEN_MASK |
+                TSI_GENCS_STPE_MASK |
+                TSI_GENCS_EOSF_MASK; // end of scan flag, set to 1 to clear
+}
+
+/*  read touch sesnor from lowto high capciatcance */
+int Touch_Scan_LH(void)
+{
+  TSI0->DATA = TSI_DATA_TSICH(10u); // chnanhel 10
+  TSI0->DATA |= TSI_DATA_SWTS_MASK;
+  int scan = (TSI0->DATA & TSI_DATA_TSICNT_MASK);
+  TSI0->GENCS |= TSI_GENCS_EOSF_MASK;
+  return scan - 100; // offset (adjust)
+}
 
 int main(void)
 {
@@ -17,21 +45,33 @@ int main(void)
   BOARD_InitDebugConsole();
 #endif
 
-  /* siwtch setup */
   SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
   PORTC->PCR[SWITCH_1_PIN] &= ~PORT_PCR_MUX_MASK;
   PORTC->PCR[SWITCH_1_PIN] |= PORT_PCR_MUX(1);
   GPIOC->PDDR &= ~(1U << SWITCH_1_PIN);
   PORTC->PCR[SWITCH_1_PIN] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
 
+  Touch_Init();
+  int lastTouchState = 0;
+
   while (1)
   {
-    if (!(GPIOC->PDIR & (1U << SWITCH_1_PIN))) // button press logic
+    int touchValue = Touch_Scan_LH();
+    int currentTouchState = (touchValue > TOUCH_THRESHOLD);
+
+    if (currentTouchState && !lastTouchState)
+    {
+      PRINTF("{ \"touchDetected\": true }\r\n");
+    }
+    lastTouchState = currentTouchState;
+
+    if (!(GPIOC->PDIR & (1U << SWITCH_1_PIN)))
     {
       PRINTF("{ \"buttonPressed\": true }\r\n");
       while (!(GPIOC->PDIR & (1U << SWITCH_1_PIN)))
-        ; // on button release
+        ;
     }
+
     __asm volatile("nop");
   }
   return 0;
--- game_interface.py
+++ game_interface.py
@@ -4,14 +4,15 @@ import json
 
 ser = serial.Serial("COM3", 115200, timeout=1)
 
-
 try:
     while True:
         if ser.in_waiting > 0:
             line = ser.readline().decode("utf-8").rstrip()
             data = json.loads(line)
-            if data["buttonPressed"]:
-                print("Move frog forward")
+            if data.get("buttonPressed", False):
+                print("button pressed")
+            if data.get("touchDetected", False):
+                print("touch detected")
         time.sleep(0.1)
 finally:
     ser.close()
