234f4f19b73b975f1151fdc998b12865b89ce509
paulresmer <resmer.paul@gmail.com>
2024-05-14 10:34:32 -0400

    update site

--- final_project0/src/App.jsx
+++ final_project0/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import kiki from '../src/assets/download.png'
+import kiki from '../src/assets/crossyroad.png'
 import diagram3 from '../src/assets/diagram3.png'
 import './App.css'
 import Typography from '@mui/material/Typography';
@@ -8,52 +8,188 @@ import Typography from '@mui/material/Typography';
 function App() {
   const [count, setCount] = useState(0)
 
+  const uartInitCode = `void uart_init() {
+    uart_config_t config;
+    UART_GetDefaultConfig(&config);
+    config.baudRate_Bps = 115200;
+    config.enableTx = true;
+    config.enableRx = true;
+    UART_Init(UART2, &config, CLOCK_GetFreq(UART2_CLK_SRC));
+}`;
+
+  const uartReadCode = `void read_score(void) {
+    if (UART0->S1 & UART_S1_RDRF_MASK) {
+        uint8_t received = UART0->D;
+        PRINTF("{ \\"scoreReceived\\": %d }\\r\\n", received);
+        SegLCD_DisplayDecimal(received);
+    }
+}`;
+
+  const touchScanCode = `int Touch_Scan_LH(void) {
+    TSI0->DATA = TSI_DATA_TSICH(10u); // using channel 10 of the TSI
+    TSI0->DATA |= TSI_DATA_SWTS_MASK; // software trigger for scan
+    int scan = (TSI0->DATA & TSI_DATA_TSICNT_MASK); // get scan result
+    TSI0->GENCS |= TSI_GENCS_EOSF_MASK; // reset end of scan flag
+    return scan - 100; // subtract offset (adjust as needed)
+}`;
+
+  const debounceCode = `if (currentTouchState != lastTouchState) {
+    debounceCounter++;
+    if (debounceCounter >= DEBOUNCE_COUNT) {
+        lastTouchState = currentTouchState;
+        debounceCounter = 0;
+        if (currentTouchState) {
+            PRINTF("{ \\"touchDetected\\": true }\\r\\n");
+        }
+    }
+} else {
+    debounceCounter = 0;
+}`;
+
+  const buttonInitCode = `SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+PORTC->PCR[SWITCH_1_PIN] &= ~PORT_PCR_MUX_MASK;
+PORTC->PCR[SWITCH_1_PIN] |= PORT_PCR_MUX(1);
+GPIOC->PDDR &= ~(1U << SWITCH_1_PIN);
+PORTC->PCR[SWITCH_1_PIN] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;
+
+PORTC->PCR[SWITCH_3_PIN] &= ~PORT_PCR_MUX_MASK;
+PORTC->PCR[SWITCH_3_PIN] |= PORT_PCR_MUX(1);
+GPIOC->PDDR &= ~(1U << SWITCH_3_PIN);
+PORTC->PCR[SWITCH_3_PIN] |= PORT_PCR_PE_MASK | PORT_PCR_PS_MASK;`;
+
+  const segLcdCode = `void SegLCD_Init(void) {
+    // LCD initialization and configuration code
+}
+
+void SegLCD_DisplayDecimal(uint16_t value) {
+    if (value > 9999) {
+        return; // display "Err" if value is greater than 4 digits
+    }
+    SegLCD_Set(value / 1000, 1);
+    SegLCD_Set((value / 100) % 10, 2);
+    SegLCD_Set((value / 10) % 10, 3);
+    SegLCD_Set(value % 10, 4);
+}`;
+
+  const frogMoveCode = `class Frog(pygame.sprite.Sprite):
+    def move(self, direction):
+        if self.can_move():
+            if direction == "UP":
+                self.rect.y -= LANE_HEIGHT
+                self.score += 1
+                encode_score = self.score.to_bytes(1, "big")
+                print(self.score)
+                ser.write(encode_score)
+            elif direction == "DOWN":
+                self.rect.y += LANE_HEIGHT
+            elif direction == "LEFT":
+                self.rect.x -= LANE_HEIGHT
+            elif direction == "RIGHT":
+                self.rect.x += LANE_HEIGHT`;
+
+  const serialPythonCode = `import serial
+ser = serial.Serial("COM3", 115200, timeout=1)
+
+while True:
+    if ser.in_waiting > 0:
+        line = ser.readline().decode("utf-8").rstrip()
+        data = json.loads(line)
+        if data.get("touchDetected"):
+            frog.move("UP")
+        if data.get("buttonPressed"):
+            frog.move("RIGHT")
+        if data.get("SW3Pressed"):
+            frog move("LEFT")`;
+
+  const updatedCodeSnippet = `encode_score = self.score.to_bytes(1, "big")
+print(self.score)
+ser.write(encode_score)`;
+
   return (
     <>
-      <div class='page'>
+      <div className='page'>
         <div>
-          <h1 class='name'>cross-c road</h1>
-          <p class='routes'> <a href="https://github.coecis.cornell.edu/ece3140-sp2024/jse77-pgr39/tree/main">jse77 & pgr39</a></p>
+          <h1 className='name'>Cross-C Road</h1>
+          <p className='routes'> <a href="https://github.coecis.cornell.edu/ece3140-sp2024/jse77-pgr39/tree/main">jse77-pgr39</a></p>
         </div>
-        <div class='haha'>
-          <a href="https://github.coecis.cornell.edu/ece3140-sp2024/jse77-pgr39/tree/main" target="_blank">
-            <img src={kiki} className="logo" alt="Vite logo" />
+        <div className='haha'>
+          <a href="https://github.coecis.cornell.edu/ece3140-sp2024/jse77-pgr39/tree/main" target="_blank" className="logo-container">
+            <img src={kiki} className="logo kiki" alt="Crossy Road logo" />
           </a>
-          <div class='about'>
-            <h3>overview</h3>
-            <p class='body'>
-              Our game is modeled off the popular mobile device game, Crossy Road. Our implementation of it will have the graphics in Python, and the game logic in C since we need to communicate with our external FRDM board.
-
-
-              As a brief overview of how the game typically works, we have a character who tries to avoid obstacles in the road and the objective of the game is to get as far as possible down the course without colliding with any of the obstacles.
-
-
-              We will interact with the board in the following ways:
-              In order to move forward, we will use the switch one, located in the top left corner of the board
-              To move horizontally to avoid obstacles, we will use the on board gyroscope, and by tilting the board, the player will be able to move the character left and right
-              The on board display will display your current score, that is how far you’ve made it in the game, and the score can range from 0 - 8888.
-
-
-            </p>
-
-            <p class='body'>
-
+          <div className='about'>
+            <h3>Overview</h3>
+            <p className='body'>
+              "Cross-C Road" is an implementation of the popular arcade/mobile game <a href="https://en.wikipedia.org/wiki/Crossy_Road">Crossy Road</a>,
+              where the player controls a character through an endless path of obstacles.
+              The objective of the game is to live for as long as possible.
+              This app uses serial communication between the FRDM-KL46Z and the user's computer via <a href="https://github.com/pyserial/">pyserial</a>,
+              an open-source serial port access library for Python. Our game is rendered on <a href="https://github.com/pygame/pygame">pygame</a>, an open source library built on top of SDL.
+              <p>We use the FRDM-KL46Z's onboard <a href="https://www.farnell.com/datasheets/1728679.pdf">capacitive touch slider</a> and user push buttons to control in-game actions such as moving the frog.
+                The microcontroller receives and processes the player's score and displays it on the segmented LCD. The game begins upon
+                detecting a touch input, and the frog navigates through lanes of moving vehicles,
+                with increasing difficulty and new stages as the player progresses.
+                If the player allows the frog to collide with a vehicle, the game resets and a new session.
+              </p>
             </p>
           </div>
         </div >
-        <div class='technical approach'>
-          <h3>technical approach</h3>
-          <p class='body'>
-            We intend to use serial communication with the host computer via the debug PRINTF functions in MCUXpresso and the pyserial package found here (https://pyserial.readthedocs.io/en/latest/). As project files print to the debug console, pyserial will read these outputs and receive them as inputs to our python program, which will contain the graphics for our game. Since we would like the user input for the game to be taken from the FRDM board, we need to code the rest of this project in C in order to communicate and receive the controls from the board. Similar to our discussion where we poll for interrupts in the case that a button was pressed, we will have our program determine when the switch on the board has been pressed, and consequently move the character forward. In order to do this, we will first enable the clock to the port which is associated with the switch. Then, we will clear the PCR Mux bits for the corresponding port. Then, we will need to set up the port as input, so that we can track the user's actions. This is done by setting it up as GPIO. Then, we will turn on pull enables, and clear the interrupt bits for the port so that we can begin with polling whether or not the switch has been pressed. Then, we will have a switch_polling function that moves the character forward if the interrupt flag is on. Afterwards, we clear the flag, and wait for the next switch to be pressed. In terms of game logic, we will largely base our game on Crossy Road, thus, we will simply check for object overlap to determine whether or not the move forward was valid. In order to avoid objects, we would also like the user to be able to use the on board gyroscope, tilting it to move the character in the respective direction. On the board itself, we have 3-axis sensors, which detect rotation, which we use their relative degree in space to determine whether to move the character to the right or left. This will be a continuous polling, and we will track if the board remains at a certain degree threshold for about half a second to see whether or not to move the character. If we detect such behavior, then, we will trigger an interrupt that calls a function that will move the character to the corresponding direction. Our other on board interaction will come in the scorekeeping feature. Each successful forward jump will increase the counter by 1, which we will display on the on board screen. As a highly unlikely possibility, if the player maxes the display out at 8888, we will print win on the board.
+        <div className='haha'>
+          <div className='about technical-approach'>
+            <h3>Technical Approach</h3>
+            <p className='body'>
+              The game leverages the FRDM-KL46Z microcontroller's onboard  <br></br>peripherals and GPIO pins to interface with the user and provide <br></br>feedback. Below is a detailed breakdown of the hardware <br></br>components and their configuration:
 
+              <h4>1. UART Communication</h4>
+              <p>
+                UART (Universal Asynchronous Receiver/Transmitter) is used for serial communication between the microcontroller and the computer. The UART interface is configured to transmit and receive data at a baud rate of 115200. This setup allows the game to send and receive messages, such as touch events and score updates, via the serial port.
+              </p>
+              <p>Specifically, the <code>uart_init</code> function in the C code initializes the UART with the desired configuration, enabling both transmission and reception:</p>
+              <pre><code>{uartInitCode}</code></pre>
+              <p>In the game loop, the microcontroller checks if data has been received through UART. If a score is received, it is displayed on the segmented LCD. This is handled by the <code>read_score</code> function:</p>
+              <pre><code>{uartReadCode}</code></pre>
+              <p>For example, when the player scores points in the game, the score is sent from the game to the microcontroller via UART:</p>
+              <pre><code>{updatedCodeSnippet}</code></pre>
+              <p>The microcontroller then updates the LCD to display the new score.</p>
 
-          </p>
+              <h4>2. Capacitive Touch Slider</h4>
+              <p>
+                The onboard capacitive touch slider of the FRDM-KL46Z is used to detect touch inputs. The touch slider is initialized and configured for capacitive sensing using the TSI (Touch Sensing Input) module. The <code>Touch_Scan_LH</code> function scans the touch sensor and processes the touch values to detect valid touch events, which start the game and move the frog upward.
+              </p>
+              <p>The <code>Touch_Scan_LH</code> function works by initiating a scan on a specific TSI channel and reading the result:</p>
+              <pre><code>{touchScanCode}</code></pre>
+              <p>In this function, a scan is triggered on channel 10 of the TSI module, and the scan result is read. The result is then adjusted by subtracting an offset to get the final touch value. This value is used to determine if a touch event has occurred by comparing it to a predefined threshold.</p>
+              <p>Debouncing is crucial to ensure that false or noisy touch readings do not trigger unintended actions. The debounce logic checks for consistent touch states over multiple readings:</p>
+              <pre><code>{debounceCode}</code></pre>
+              <p>In this debounce implementation, the current touch state is compared with the last touch state. If they are different, a debounce counter is incremented. If the counter exceeds a certain threshold (DEBOUNCE_COUNT), the touch state is considered valid, and the counter is reset. If the touch states are consistent, the counter is reset to zero. This ensures that only stable touch readings are processed.</p>
 
-          <p class='body'>
-            <img src={diagram3} className="diagram3" alt="Vite logo" />
-          </p>
+              <h4>3. Push Buttons</h4>
+              <p>
+                The FRDM-KL46Z has push buttons connected to specific GPIO (General Purpose Input/Output) pins. These buttons are used to move the frog left and right. The GPIO pins are configured with pull-up resistors, and button presses are detected by checking the pin states. When a button is pressed, an event is sent to the game to move the frog accordingly. The code sets up the GPIO pins for the buttons, enables pull-up resistors, and checks for button presses within the main loop:
+              </p>
+              <pre><code>{buttonInitCode}</code></pre>
+
+              <h4>4. Segment LCD Display</h4>
+              <p>
+                The segmented LCD on the FRDM-KL46Z is used to display the player's score. The LCD is initialized and configured with the necessary segment and backplane pins using the <code>SegLCD_Init</code> function. The score is displayed by updating the appropriate segments on the LCD through the <code>SegLCD_DisplayDecimal</code> function, which translates numerical values into segment patterns displayed on the LCD. This allows the player to see their current score in real-time:
+              </p>
+              <pre><code>{segLcdCode}</code></pre>
+
+              <h4>5. Game Logic and Pygame</h4>
+              <p>
+                The game logic is implemented in Python using the Pygame library. Pygame is used to create the game window, render graphics, and handle user inputs. The frog character and vehicles are represented as sprites, and their movements and interactions are managed by the game loop. The game starts upon detecting a touch input from the microcontroller. The frog moves through lanes of vehicles, with the difficulty increasing as the game progresses. If the frog collides with a vehicle, the game resets, and a new session begins. The main loop in Pygame handles event processing, game state updates, and rendering:
+              </p>
+              <pre><code>{frogMoveCode}</code></pre>
+
+              <h4>6. Serial Communication in Python</h4>
+              <p>
+                The serial communication between the microcontroller and the Python game is handled using the pyserial library. The game continuously reads data from the serial port to process touch and button events. The player's score is sent from the game to the microcontroller, which updates the segmented LCD display accordingly. The Python code sets up the serial connection, reads incoming data, and parses the JSON messages to trigger in-game actions and update the score display:
+              </p>
+              <pre><code>{serialPythonCode}</code></pre>
+            </p>
+          </div>
+          <img src={diagram3} className="diagram3" alt="Technical Diagram" />
         </div>
-        <div class='card' id="contact">
+        <div className='card' id="contact">
           {/* <button onClick={() => setCount((count) => count + 1)}>
             {count} clicks wasted
           </button> */}
@@ -63,7 +199,7 @@ function App() {
         <p className="read-the-docs">
           final project for ece3140/cs3420 sp24
         </p>
-      </div>
+      </div >
     </>
   )
 }
